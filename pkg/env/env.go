5a23b7ae27e4186adfff50dd2e4941c6271594a8
Rick Bassham <brodrick.bassham@gmail.com>
2019-09-22 14:33:15 -0400

    feat(env): add team name to common config

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -9,6 +9,7 @@ import (
 // Config represents the common environment variables needed for all apps.
 type Config struct {
 	AppName         string    `env:"APP_NAME,required"`
+	TeamName        string    `env:"TEAM_NAME,required"`
 	Environment     string    `env:"APP_ENV,required"`
 	BuildDate       time.Time `env:"BUILD_DATE"`
 	BuildGitHash    string    `env:"BUILD_GIT_HASH,required"`
